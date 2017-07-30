15988e5df9c63a70326ab7b6838e6b34f0f70213
Иван Ботнарь <ivan.icharts@gmail.com>
2017-07-30 11:30:27 +0300

    Added: bg, media queries

--- src/memory-mosaic/components/Row.jsx
+++ src/memory-mosaic/components/Row.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 export default ({keyValue, height, children}) => (
 	<div
 		style={{
-			display: 'flex',
-  		justifyContent: 'center',
-	    width: '100%',
+
 	    height: `${100 / height}%`
 		}}
+		className='row'
 		key={keyValue}>
 		{children}
 	</div>
