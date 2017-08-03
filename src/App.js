fdb941b03edac422f2be3eff9a208e73176e00e5
Иван Ботнарь <ivan.icharts@gmail.com>
2017-08-04 00:02:00 +0300

    Started home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import './App.scss'
 
 import MemoryMosaic from './memory-mosaic'
-
+import Home from 'pages/home'
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <MemoryMosaic />
+        <Home />
       </div>
     )
   }
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react'
+
+import Header from './components/header'
+
+import './style.scss'
+
+class Home extends PureComponent {
+
+	render = () => (
+		<div className='home-wrapper'>
+			<Header />
+			asd
+		</div>
+	)
+}
+
+export default Home
