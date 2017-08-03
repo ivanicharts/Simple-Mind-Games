f836b5148b0a29fd72a2d8a2603434737d36bd82
ivanicharts <ivan.icharts@gmail.com>
2017-08-03 17:31:25 +0300

    Added field generator on server

--- server/src/memory-mosaic/controller.js
+++ server/src/memory-mosaic/controller.js
@@ -1,4 +1,13 @@
 import { GET } from '../../constants/methods'
+import config, { levels } from '../../../src/config/memory-mosaic.json'
+import state from './state'
+
+import {
+  WIN_CELL, WRONG_CELL, CELL,
+  CELL_COUNT, FIELD_SIZE, EMPTY_CELL, MS
+} from './../../../src/memory-mosaic/constants'
+
+export const getHashKey = (x, y) => String(x) + y
 
 export default {
   getAll: {
@@ -13,7 +22,33 @@ export default {
     method: GET,
     route: '/field',
     controller(req, res) {
-      res.send('brand new field!')
+      const {currentLevel} = state
+      const hash = {}
+      const size = levels[currentLevel][FIELD_SIZE]
+      const field = new Array(size).fill(0)
+      const cellCount = levels[currentLevel][CELL_COUNT]
+
+      field.forEach((e, i, a) => (field[i] = new Array(size).fill(0)))
+
+      let i = 0
+      while (i < cellCount) {
+        const x = ~~(Math.random() * size)
+        const y = ~~(Math.random() * size)
+        const key = getHashKey(x, y)
+
+        field[x][y] = CELL
+
+        if (!(key in hash)) (hash[key] = EMPTY_CELL,  i++)
+
+        // Infinite loop protection temporary
+        if (i > 250) break
+      }
+
+      // this.setState({field, hash, visible: true, guessedCells: 0, ready: true})
+      // setTimeout(() => this.setState({ready: false}), config.preloadTime * MS + 100)
+      // setTimeout(() => this.setState({visible: false}), config.preloadTime * MS + config.visibleTime * MS)
+
+      res.send(field)
     }
   },
 
