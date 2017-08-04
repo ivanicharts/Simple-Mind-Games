f17fc4f0129705757dceadfeb902653b1388fa52
ivanicharts <ivan.icharts@gmail.com>
2017-08-04 09:20:07 +0300

    Little fix

--- src/pages/home/components/header.jsx
+++ src/pages/home/components/header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 export default () => (
-	<header className='home-header'>
-		<h1 className='logo'> mind games </h1>
-		<div className='header-text'>
-			<span>
-				Thank you for coming
-			</span>
-		</div>
-	</header>
+  <header className='home-header'>
+    <h1 className='logo'> mind games </h1>
+    <div className='header-text'>
+      <span>
+        Thank you for coming
+      </span>
+    </div>
+  </header>
 )
