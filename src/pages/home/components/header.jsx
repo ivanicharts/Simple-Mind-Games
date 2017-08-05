f200dadd02ff4c7b32e4bd9d0f05934f2dfe5af8
Иван Ботнарь <ivan.icharts@gmail.com>
2017-08-05 09:33:27 +0300

    Added particles in header on Home Page

--- src/pages/home/components/header.jsx
+++ src/pages/home/components/header.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import Particles from 'react-particles-js'
+
+const particlesParams = {
+	particles: {
+		number: {
+			value: 35,
+			density: {enable: false}
+		},
+		color: {
+			value: '#64367F'
+		},
+		size: {value: 5, random: true},
+		line_linked: {
+			enable: false
+		}
+	}
+}
 
 export default () => (
   <header className='home-header'>
     <h1 className='logo'> mind games </h1>
     <div className='header-text'>
+    	<Particles 
+    		height={400}
+    		params={particlesParams}
+    		style={{
+    			maxWidth: '100%'
+    		}}
+    	/>
       <span>
-        Thank you for coming
+        Thank you for coming :)
       </span>
     </div>
   </header>
