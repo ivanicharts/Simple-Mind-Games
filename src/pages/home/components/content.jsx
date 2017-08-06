057209e16e1955c7642a41751f7b2a650987de02
Иван Ботнарь <ivan.icharts@gmail.com>
2017-08-06 19:39:21 +0300

    Added image to games

--- src/pages/home/components/content.jsx
+++ src/pages/home/components/content.jsx
@@ -15,50 +15,58 @@ export default class extends PureComponent {
 	openHandler = (game) => () => this.setState({game, showModal: true})
 	closeHandler = () => this.setState({showModal: false})
 
-	render = () => (
-		<main className='content-wrapper'>
-		<section>
-			Available games:
-		</section>
+	render = () => {
+    const {game} = this.state
 
-		
-		{
-      this.state.showModal &&
-			<Modal onClose={this.closeHandler}>
-        <div className='item-info'>
-          {
-            this.state.game.image ?
-            <img src={require('assets/images/mosaic-field.png')} /> :
-            <Icon className='item-caption' name='empire' />
-          }
-          <p className='game-name'>{this.state.game.label}</p>
+    return (
+      <main className='content-wrapper'>
+      <section>
+        Available games:
+      </section>
 
-          <div className='block'>
-            <h4 className='game-description'>Description:</h4>
-            <p className='text'>{this.state.game.description}</p>
-          </div>
-          
-          <div className='block'>
-            <h4 className='game-description'>Rules:</h4>
-            <p className='text'>{this.state.game.rules}</p>
-          </div>
-                  
-          {this.state.game.singleplayer && <Link to='/memory'><button className='select-btn'>Single player</button></Link>}
-          {this.state.game.multiplayer && <button className='select-btn'>Multiplayer</button>}
-        </div>
-	    </Modal>
-    }
+      
+      {
+        this.state.showModal &&
+        <Modal onClose={this.closeHandler}>
+          <div className='item-info'>
+            {
+              this.state.game.image ?
+              <img src={game.image} /> :
+              <Icon className='item-caption' name='empire' />
+            }
+            <p className='game-name'>{game.label}</p>
 
-		<section className='list'>
-			{
-        games.map((g) => (
-          <div onClick={this.openHandler(g)} className='list-item'>
-            <Icon className='item-caption' name='first-order' />
-            <div>{g.label}</div>
+            <div className='block'>
+              <h4 className='game-description'>Description:</h4>
+              <p className='text'>{game.description}</p>
+            </div>
+            
+            <div className='block'>
+              <h4 className='game-description'>Rules:</h4>
+              <p className='text'>{game.rules}</p>
+            </div>
+                    
+            {game.singleplayer && <Link to='/memory'><button className='select-btn'>Single player</button></Link>}
+            {game.multiplayer && <button className='select-btn'>Multiplayer</button>}
           </div>
-        ))
+        </Modal>
       }
-		</section>
-	</main>
-	)
+
+      <section className='list'>
+        {
+          games.map((g) => (
+            <div onClick={this.openHandler(g)} className='list-item'>
+              {
+                g.image ? 
+                <img className='item-caption dark-bg' src={g.image} /> :
+                <Icon className='item-caption' name='first-order' />
+              }
+              <div>{g.label}</div>
+            </div>
+          ))
+        }
+      </section>
+    </main>
+    )
+  }
 }
