ad136e34eab478b5c90df923928e2e080810f3d5
Vadim Nicolai <v.nicolai@developmentaid.org>
2017-08-07 16:38:06 +0300

    Removing client dependency.

--- config/assets/default.js
+++ config/assets/default.js
@@ -3,55 +3,6 @@
 /* eslint comma-dangle:[0, "only-multiline"] */
 
 module.exports = {
-  client: {
-    lib: {
-      css: [
-        // bower:css
-        'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-        'public/lib/angular-ui-notification/dist/angular-ui-notification.css'
-        // endbower
-      ],
-      js: [
-        // bower:js
-        'public/lib/angular/angular.js',
-        'public/lib/angular-animate/angular-animate.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-        'public/lib/ng-file-upload/ng-file-upload.js',
-        'public/lib/angular-messages/angular-messages.js',
-        'public/lib/angular-mocks/angular-mocks.js',
-        'public/lib/angular-resource/angular-resource.js',
-        'public/lib/angular-ui-notification/dist/angular-ui-notification.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        // endbower
-      ],
-      tests: ['public/lib/angular-mocks/angular-mocks.js']
-    },
-    css: [
-      'modules/*/client/{css,less,scss}/*.css'
-    ],
-    less: [
-      'modules/*/client/less/*.less'
-    ],
-    sass: [
-      'modules/*/client/scss/*.scss'
-    ],
-    js: [
-      'modules/core/client/app/config.js',
-      'modules/core/client/app/init.js',
-      'modules/*/client/*.js',
-      'modules/*/client/**/*.js'
-    ],
-    img: [
-      'modules/**/*/img/**/*.jpg',
-      'modules/**/*/img/**/*.png',
-      'modules/**/*/img/**/*.gif',
-      'modules/**/*/img/**/*.svg'
-    ],
-    views: ['modules/*/client/views/**/*.html'],
-    templates: ['build/templates.js']
-  },
   server: {
     gulpConfig: ['gulpfile.js'],
     allJS: ['server.js', 'config/**/*.js', 'modules/*/server/**/*.js'],
